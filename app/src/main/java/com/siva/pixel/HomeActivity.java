1b86adc5b1874da25e2444fb346e54cc5dd759e5
Siva <pingsivaram@gmail.com>
2017-06-17 12:41:41 +0530

    First commit: Shree Ganeshaya namahah

---
+++ app/src/main/java/com/siva/pixel/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.siva.pixel;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by siva on 17/06/17.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
